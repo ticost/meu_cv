0755d158521a4cbf6d5578551235667e8a6512d5
ticost <51927005+ticost@users.noreply.github.com>
2025-11-07 10:44:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ E-mail: silmar.tolotto@gmail.com
 Celular: (11) 9 8928-1468
 Endereço: Rua Cajati, 345 Freguesia do Ó - CEP 02729-040 São Paulo - SP
 Nascimento: 09 de março de 1969
-LinkedIn: https://www.linkedin.com/in/silmartolottoa227716
+LinkedIn: www.linkedin.com/in/silmar-tolotto-0a227716
 
 RESUMO PROFISSIONAL:
 Gerente de Projetos, Professor e Analista de Infraestrutura de TI, organizado e orientado a resultados. 
@@ -629,3 +629,4 @@ elif menu == "Atividades e Voluntariado":
 
 st.markdown("---")
 st.caption("Desenvolvido com ❤️ em Streamlit | © 2025 - Silmar Tolotto")
+
